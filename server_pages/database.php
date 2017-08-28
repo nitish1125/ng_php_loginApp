d43a55f596edb98546378156f96ac93332542d09
nitish chavan <nitishkchavan@gmail.com>
2017-08-28 19:14:22 +0530

    commit 28th August

--- server_pages/database.php
+++ server_pages/database.php
@@ -21,9 +21,22 @@
 	class Database
 	{
 		
-		function __construct(argument)
-		{
-			# code...
+		private $conn= null;
+
+		public function __construct(){
+			$connnection = new Connnection();
+			$this->conn=$connnection->getConnection();
+		}
+
+		public function aunthenticate($username,$password){
+			$password=md5($username);
+			$query="select * from user where email= '".$username."'and password= '".$password."'limit 1";
+			$result=$this->conn->query($query);
+
+			if($result->num_rows > 0){
+				return true;
+			}
+			return false;
 		}
 	}
  ?>
--- server_pages/server.php
+++ server_pages/server.php
@@ -1,10 +1,17 @@
 <?php 
 
+require('../server_pages/database.php');
+
 $form_data=file_get_contents("php://input");
 
 $data=json_decode($form_data);
 
 $what_to_do=$data->what_to_do;
 
-echo $what_to_do=$what_to_do;
+if($what_to_do == "authenticate_user"){
+	$db_handler = new Database();
+
+	echo $db_handler->aunthenticate($data->email,$data->password);
+}
+
  ?>
